f705e43486be1c68f0b9f10d32196cd3921d7614
Your Name <you@example.com>
2019-09-12 11:50:23 +0700

    firs commit

---
+++ src/ArrayTemanSederet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ARVINA
+ */
+public class ArrayTemanSederet {
+
+    public static void main(String[] args) {
+        // membuat array
+
+        String[] teman = {"Bayu", "Amy", "Necha", "Devon", "Lukas", "fara"};
+        
+        // mengambil data array
+        System.out.println("Teman pertama saya " + teman[2] + " kedua " + teman[1] + " ketiga " + teman[5] );
+    }
+}
---
+++ src/ChallengeArray01.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ARVINA
+ */
+import java.util.Scanner;
+
+public class ChallengeArray01 {
+    public static void main (String[] args) {
+       String[] nama   = {"Necha Syifa Syafitri" , "In Amy fadliyyah" , "Rensi meila yulvinata" , "Adela Aishwara Putri" , "Sofia izaaz jauzza" , "Nurisa Rahmi Audining" , "Tembang Raras Kusumawuri" , "Fidya Maharani" , "Diva Mareta" , "Naisya Najmi"};
+       String[] alamat = {"Malang", "Tuban" , "Kediri" , "Magetan" , "Nganjuk" , "Tulungagung" , "Ngawi" , "Turen" , "Turen" , "TulungAgung"};
+       String[] NoHp   = {"082132236984 " , "081243294691" , "082334929755" , "081382506640" , "0859691458705" , "081336278584" , "085235133444" , "085235601746" , "081252552982" , "082142004434"};
+       String[] hobi   = {"Memberi makan kucing" , "Tidur" , "Menggambar" , "Olahraga" , "Makan", "Nyanyi", "Makan" , "Masak" , "Ghibah" , "Nyanyi"};
+       String[] ig     = {"@nechasyifa" ,"@amytaakryanti" , "@rensimly" , "@adella.aish" , "@sofia_jauzaa" , "@ris.risol" , "@trarask" , "@fidiaamahranii" , "@diva.marettaa" , "@naisya.ca"};
+       
+       //output
+       System.out.println("=== Biodata teman pertama ===");
+       System.out.println("Nama : " + nama[0]);
+       System.out.println("Alamat Asal : "+ alamat[0]);
+       System.out.println("No.HP :" + NoHp [0]);
+       System.out.println("hobi : " + hobi[0]);
+       System.out.println("Instagram : " + ig[0] + " === Jangan Lupa Follow,=== ");
+       System.out.println(" ");
+       
+       System.out.println("===Biodata Teman kedua===");
+       System.out.println("Nama : " + nama[4]);
+       System.out.println("Alamat Asal : "+ alamat[4]);
+       System.out.println("No.HP :" + NoHp [4] );
+       System.out.println("hobi : " + hobi[4]);
+       System.out.println("Instagram : " + ig[4] + " === Jangan Lupa Follow ===)");
+       System.out.println(" ");
+    }
+    }
+
+
+    
+      
+
